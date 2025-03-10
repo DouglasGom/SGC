5aa5b74452afc33279073cef56b46d3c47a48adf
Douglas Gomes De Campos <douglasgomcampos@gmail.com>
2025-03-10 11:42:38 -0300

    Delete src directory

--- src/components/pages/Departamentos/Departamentos.js
+++
@@ -1,7 +0,0 @@
-import styles from './Departamentos.module.css'
-
-function Departamentos() {
-        return <h1 className={styles.title}>Departamentos</h1>
-}
-
-export default Departamentos;
--- src/components/pages/Dispositivos/Dispositivos.js
+++
@@ -1,7 +0,0 @@
-import styles from './Dispositivos.module.css'
-
-function Dispositivos() {
-    return <h1 className={styles.title}>Dispositivos</h1>
-}
-
-export default Dispositivos;
